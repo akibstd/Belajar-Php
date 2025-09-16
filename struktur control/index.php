94a0c8bbd09ca047875d86de167ffa6faf52dac4
Akib Supandi <akibsupandi14@gmail.com>
2025-09-16 21:36:34 +0700

    p3

---
+++ struktur control/index.php
@@ -0,0 +1,65 @@
+<?php
+ 
+ echo"<h1>-------- if------------- <h1>";
+// if else di php 
+$nilai = 80;
+if ($nilai > 75) {
+   echo '<h1> nilai a </h1>';
+} else {
+   'nilai b';
+}
+
+//switch case 
+
+echo"<h1>--------------- switchs ------------------ <h1>";
+$warna = "red";
+switch ($warna == 'red') {
+   case "red";
+      echo '<h1> warna merah </h1>';
+      break;
+   case "blue":
+      echo "Your favorite color is blue!";
+      break;
+   case "green":
+      echo "Your favorite color is green!";
+      break;
+
+}
+
+echo"<h1>---------------- while-----------------  <h1>";
+// ⁡⁢⁣⁣while loop⁡ 
+$i = 1; // 
+while ($i < 3) {
+   if ($i === 3)
+      break;
+   echo '<h1> while </h1>';
+   $i++;
+
+}
+
+echo"<h1>----------------- Dowhile---------------------- <h1>";
+// ⁡⁢⁣⁣do while loop  
+$i = 0;
+do {
+   echo "<h1>  $i </h1>";
+   $i++;
+} while ($i < 6);
+
+
+ 
+ for($number =0 ;$number < 11; $number ++){
+   echo "<h1> cetak $number </h1>";
+ }
+
+echo"<h1>--------------------- foreach-----------------  <h1>";
+  
+$colors = array("red", "green", "blue", "yellow");  
+foreach ($colors as $x) { 
+echo "<h1>$x </h1>"; 
+} 
+
+
+
+
+
+?>
---
+++ struktur control/readmi .md
@@ -0,0 +1,13 @@
+
+ ⁡⁢⁣
+⁣if else di gunakan untuk mengecek jika nilai lebih dari 80 maka tampikan nilai a jika niai di bawah 80 b
+
+untuk contoh swith case ini saya gunakan untuk mengecek warna jika warna nya merah maka dia akan mengecek kondidi case mana yang bernilai merah lau di print
+
+dan untuk while loop saya  mengkondisikan apa kai variable $i kurang dari 3  dan saya tambahkan di dalam while pengkondisian lain dengan if jika nilai nya identik 3 maka hentikan perulangan 
+
+ dan untuk do while saya tampilkan dulu apa yang ingin saya print lalu saya lakukan perulangan
+
+ untuk for loop saya buat varialbe number =0 jika number kurang dari 11 maka akan di increment langkah elajutnya mencetak $number
+
+ foreach di gunakan untuk mengulang perulangan dari array⁡
