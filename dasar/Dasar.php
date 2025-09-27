41d5be522fbe09b6c8a4ae5753a943da952b2f0d
Akib Supandi <akibsupandi14@gmail.com>
2025-09-27 14:07:23 +0700

    p3

--- dasar/Dasar.php
+++ dasar/Dasar.php
@@ -3,20 +3,42 @@ $data1 = 56;
 echo "<b> file php pertama saya <br> ini php dalah html </b>";
 echo "<h1> nilai 1 adalah $data1 </h1>";
 
-$nilai1 = 10;
-$nilai2 = 20;
-$hasil = $nilai1 + $nilai2;
+// $nilai1 = 10;
+// $nilai2 = 20;
+// $hasil = $nilai1 + $nilai2;
 
-$buku= 17500;
-$mouse=15000;
-$plasdisk=70000;
-$pulpen=321900;
+// $buku= 17500;
+// $mouse=15000;
+// $plasdisk=70000;
+// $pulpen=321900;
+// $diskon=0.5;
+// $hasil1=$buku*2;
+// $hasil2=$mouse*5;
+// $hasil3=$pulpen*3;
+// $total=$hasil1 + $hasil2 + $plasdisk + $hasil3;
+// $totallan= $total - $diskon;
 
-$hasil1=$buku*2;
-$hasil2=$mouse*5;
-$hasil3=$pulpen*3;
-$total=$hasil1 + $hasil2 + $plasdisk + $hasil3;
-$totallan= $total - 5;
+$jumlah=[1,2,3,4,5];
+$barang=[
+  'buku'=>17500,
+   'mouse'=>15000,
+   'pulpen'=>22000,
+   'plasdisk'=>30000
+];
+
+$hasilbuku=$barang['buku'] * $jumlah[1];
+$hasilMouse=$barang['mouse'] * $jumlah[4];
+$hasilPlasdisk=$barang['plasdisk'] *$jumlah[0];
+$hasilPulpen=$barang['pulpen'] *$jumlah[2];
+
+
+$total=$hasilbuku +$hasilMouse +$hasilPlasdisk + $hasilPulpen;
+$diskon= 0.05;
+
+$totalAkhir=$total - $diskon;
+
+
+
 
 
 
@@ -52,38 +74,41 @@ $totallan= $total - 5;
             <tr>
               <td>Buku</td>
                <td> 2</td>
-               <td>17000</td>
-               <td><?= $hasil1 ?></td>
+               <td><?=$barang['buku'] ?></td>
+               <td><?= $hasilbuku ?></td>
             </tr>
 
             <tr>
               <td>Mouse</td>
                <td> 5</td>
-               <td>3000</td>
-               <td><?= $hasil2 ?></td>
+               <td><?=$barang['mouse']?></td>
+               <td><?= $hasilMouse ?></td>
             </tr>
 
             <tr>
               <td>plashisk</td>
                <td> 1</td>
-               <td>70000</td>
-               <td><?= $plasdisk ?></td>
+                <td><?=$barang['plasdisk']?></td>
+               <td><?= $hasilPlasdisk ?></td>
             </tr>
              
              <tr>
               <td>pulpen</td>
                <td> 3</td>
-               <td>22300</td>
-               <td><?= $hasil3 ?></td>
+               <td><?=$barang['pulpen'] ?></td>
+               <td><?= $hasilPulpen ?></td>
             </tr>
             <tr>
-                <td>Total </td>
+                <td colspan="3">Total </td>
                 <td>   <?= $total ?> <tr>
             </tr>
               <tr>
-                <td>Diskon 0,5</td>
-                 <td>Total Bayar</td>
-                 <td><?= $totallan ?></td>
+                <td colspan="3">Diskon 5%</td>
+                <td ><?= $diskon ?></td>
+              </tr>
+              <tr>
+                <td colspan="3">Total Bayar</td>
+                 <td><?= $totalAkhir ?></td>
               </tr>
     </table>
 </body>
