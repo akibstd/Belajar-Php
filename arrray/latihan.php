ce928ed7c1257a8107e6091eadf339bc154c2307
Akib Supandi <akibsupandi14@gmail.com>
2025-09-29 23:17:28 +0700

    sellf learn

--- arrray/latihan.php
+++ arrray/latihan.php
@@ -1,4 +1,43 @@
 <?php 
-$judulBuku=['buku'=>'laskarpelangi',
-             ];
+ $angka = 12;
 
+    
+    $angkaKali = array(15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1">
+       <thead>
+        <tr>
+            <td>angka1</td>
+            <td>operator</td>
+             <td>angka2</td>
+             <td>hasil</td>
+        </tr>
+       </thead>
+
+         
+         <?php foreach($angkaKali as  $angka2): ?>
+            <?php  $hasil=$angka * $angka2 ?>
+            <tr>
+                <td><?=$angka?></td>
+                <td>kali</td>
+                 <td> <?=$angka2 ?></td>
+                 <td><?= $hasil ?></td>
+                 
+            </tr>
+       </tbody>
+
+           <tbody>
+       <?php endforeach ?>
+    </table>
+</body>
+</html>
---
+++ learn-Myself/dasar/dasar.php
@@ -0,0 +1,15 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php $variable=10 ?>
+    <?="<h1>$variable </h1>" ?>
+</body>
+</html>
